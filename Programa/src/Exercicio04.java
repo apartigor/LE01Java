0ad0ee29c1102c0b8ad287e09d7b553ca5bf4ca1
apartigor <contact.jojigor@gmail.com>
2024-03-13 10:39:48 -0300

    Exercicio 4

---
+++ Programa/src/Exercicio04.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Exercicio04 {
+    public void executar() {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Digite o primeiro numero:");
+        int num1 = sc.nextInt();
+        System.out.println("\nDigite o segundo numero:");
+        double num2 = sc.nextDouble();
+
+        double soma = num1 + num2;
+        double subt = num1 - num2;
+        double mult = num1 * num2;
+        double div = num1 / num2;
+
+        System.out.println("A soma total deu : " + soma);
+        System.out.println("A subtraçao total deu : " + subt);
+        System.out.println("A multiplicaçao total deu : " + mult);
+        System.out.println("A divisao total deu : " + div);
+
+        sc.close();
+    }
+}
