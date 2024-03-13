b5930c1d9ad5d20bb92148f925991bf58d0f6a48
apartigor <contact.jojigor@gmail.com>
2024-03-13 10:41:40 -0300

    Exercicio 6

---
+++ Programa/src/Exercicio06.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Exercicio06 {
+    public void executar() {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Digite a temperatura em graus Celsius:");
+        double celsius = sc.nextDouble();
+
+        double fahrenheit = (9 * celsius + 160) / 5;
+
+        System.out.printf("A temperatura em graus Fahrenheit é: %.2f\n", fahrenheit);
+
+        sc.close();
+    }
+}
