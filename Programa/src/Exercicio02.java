ab059e1eab4f83ecbd04c3a5af91b8128763bcf0
apartigor <contact.jojigor@gmail.com>
2024-03-13 10:39:00 -0300

    Exercicio 2

---
+++ Programa/src/Exercicio02.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Exercicio02 {
+    public void executar() {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Digite o primeiro numero:");
+        int num1 = sc.nextInt();
+        System.out.println("\nDigite o segundo numero:");
+        double num2 = sc.nextDouble();
+
+        double soma = num1 + num2;
+
+        System.out.println("A soma total deu : " + soma);
+
+        sc.close();
+    }
+}
